08d8530aac6feda199922ce26640376c7d395dfd
bindu-e <127506733+bindu8601@users.noreply.github.com>
2024-02-06 11:06:06 +0530

    adding comments and likes section

--- frontend/src/components/PostCard.jsx
+++ frontend/src/components/PostCard.jsx
@@ -1,6 +1,9 @@
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import { NoProfile } from "../assets";
+import moment from "moment";
+import { BiComment, BiLike, BiSolidLike } from "react-icons/bi";
+import { MdOutlineDeleteOutline } from "react-icons/md";
 
 const PostCard = ({ post, user, deletePost, likePost }) => {
   const [showAll, setShowAll] = useState(0);
@@ -9,6 +12,7 @@ const PostCard = ({ post, user, deletePost, likePost }) => {
   const [loading, setLoading] = useState(false);
   const [replyComments, setReplyComments] = useState(0);
   const [showComments, setShowComments] = useState(0);
+  const getComments = async () => {};
   console.log({ post });
   return (
     <div className="mb-2 bg-primary p-4 rounded-xl">
@@ -20,6 +24,76 @@ const PostCard = ({ post, user, deletePost, likePost }) => {
             className="w-14 h-14 object-cover rounded-full"
           />
         </Link>
+        <div className="w-full flex justify-between">
+          <div className="">
+            <Link to={"/profile/" + post?.userId?._id}>
+              <p className="font-medium text-lg text-ascent-1">
+                {post?.userId?.firstName} {post?.userId?.lastName}
+              </p>
+            </Link>
+            <span className="text-ascent-2">{post?.userId?.location}</span>
+          </div>
+          <span>{moment(post?.createdAt ?? "2023-05-25").fromNow()}</span>
+        </div>
+      </div>
+      <div>
+        <p className="text-ascent-2">
+          {showAll === post?._id
+            ? post?.description
+            : post?.description.slice(0, 300)}
+          {post?.description?.length > 301 && showAll === post?._id ? (
+            <span
+              className="text-blue ml-2 font-medium cursor-pointer"
+              onClick={() => setShowAll(0)}
+            >
+              {" "}
+              Show Less
+            </span>
+          ) : (
+            <span
+              className="text-blue ml-2 font-medium cursor-pointer"
+              onClick={() => setShowAll(post?._id)}
+            >
+              {" "}
+              Show more
+            </span>
+          )}
+        </p>
+        {post?.image && (
+          <img
+            src={post?.image}
+            alt="posts"
+            className="w-full mt-2 rounded-lg"
+          />
+        )}
+      </div>
+      <div className="mt-4 flex justify-between items-center px-3 py-2 text-ascent-2 text-base border-t border-[#66666645]">
+        <p className="flex gap-2 items-center text-base cursor-pointer">
+          {post?.likes?.includes(user?._id) ? (
+            <BiSolidLike size={20} color="blue" />
+          ) : (
+            <BiLike size={20} />
+          )}
+          {post?.likes?.length} Likes
+        </p>
+        <p
+          className="flex gap-2 items-center text-base cursor-pointer"
+          onClick={() => {
+            setShowComments(showComments === post._id ? null : post._id);
+            getComments(post?._id);
+          }}
+        >
+          <BiComment size={20} />
+          {post?.comments?.length} Comments
+        </p>
+        {user?._id === post?.userId?._id && (
+          <div
+            className="flex gap-1 items-center text-base text-ascent-1 cursor-pointer"
+            onClick={() => deletePost(post?._id)}
+          >
+            <MdOutlineDeleteOutline size={20} />
+          </div>
+        )}
       </div>
     </div>
   );
